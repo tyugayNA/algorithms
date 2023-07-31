7163bce07affbdd95c8a27a95dbc78b14a0362e4
Tyugay Nikolay <tyugay@t8.ru>
2023-07-31 15:42:03 +0300

    week1 start task Percolation

---
+++ hello/Percolation.java
@@ -0,0 +1,43 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class Percolation {
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return true;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return true;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return true;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
+}
