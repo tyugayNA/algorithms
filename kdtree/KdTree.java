ea330ddf61fe9fb487e2a4005d691ad14a00e40f
Tyugay Nikolay <tyugay.na@gmail.com>
2023-10-25 09:10:27 +0300

    week-5 first version of PointSET implementation

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -1,19 +1,32 @@
+import edu.princeton.cs.algs4.RectHV;
+
 import java.awt.geom.Point2D;
-import java.util.TreeSet;
 
 public class KdTree {
-    private TreeSet<Point2D> points;
 
     public KdTree() {
-        points = new TreeSet<>();
+
     }
 
     public boolean isEmpty() {
-        return points.isEmpty();
+        return false;
     }
 
     public int size() {
-        return points.size();
+        return 0;
+    }
+
+    public void insert(Point2D point) {
+        if (point == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static class Node {
+        private Point2D p; // the point
+        private RectHV rect; // the rectangle
+        private Node lb; // left bottom
+        private Node rt; // right top
     }
 
     public static void main(String[] args) {
--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -1,5 +1,10 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PointSET {
     private SET<Point2D> points;
@@ -14,6 +19,53 @@ public class PointSET {
 
     public int size() {
         return points.size();
+    }
+
+    public void insert(Point2D point) {
+        if (point == null) {
+            throw new IllegalArgumentException();
+        }
+        if (!points.contains(point)) {
+            points.add(point);
+        }
+    }
+
+    public boolean contains(Point2D p) {
+        return points.contains(p);
+    }
+
+    public void draw() {
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        for (Point2D p : points) {
+            p.draw();
+        }
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        List<Point2D> list = new ArrayList<>();
+        for (Point2D p : points) {
+            if (rect.contains(p)) {
+                list.add(p);
+            }
+        }
+        return list;
+    }
+
+    public Point2D nearest(Point2D point) {
+        if (isEmpty()) {
+            return null;
+        }
+        Point2D min = point;
+        double distance = Double.MAX_VALUE;
+
+        for (Point2D p : points) {
+            if (distance > point.distanceTo(p)) {
+                distance = point.distanceTo(p);
+                min = p;
+            }
+        }
+        return min;
     }
 
     public static void main(String[] args) {
